2a6d279f2e24e5e7ba7fad0e15d5b39f72866635
atecan00 <c.ates@unibas.ch>
2021-08-25 15:55:09 +0200

    simulate distributional effects

--- main.py
+++ main.py
@@ -29,15 +29,15 @@ results = np.zeros((2, q_arr.size))
 start = time.time()
 
 for i in range(d_mwp_arr.size):
-    seed = 1232123123
+    seed = 1231232
     rd.seed(seed)
     set_seed(seed)
-    data_mat, w_dist_mat = run(T=T, alpha_2=0.25, N_good=4, lambda_LM=10, sigma_m=0.35, sigma_w=0.4, sigma_delta=0.0001,
+    data_mat, w_dist_mat = run(T=T, alpha_2=0.25, N_good=4, lambda_LM=10, sigma_m=0.35, sigma_w=0.45, sigma_delta=0.0001,
                                lambda_F=0.5, lambda_H=1.0, F=160, H=H, N_app=4, eta=1.5, min_w_par=0.4,
                                d_min_w_par=d_mwp_arr[i], change_t=600, W_u=1, Ah=1, tol=1e-14)
 
     q_mat = get_q_vals(q_arr, w_dist_mat)
-    q_vals = q_mat[-300:,:].mean(axis=0)
+    q_vals = q_mat[-100:,:].mean(axis=0)
     results[i,:] = q_vals
 
 end = time.time()
@@ -47,33 +47,12 @@ plt.plot(results[1] - results[0])
 plt.show()
 
 
-sns.distplot(np.log(w_dist_mat[250, w_dist_mat[250] > 0]))
+sns.distplot(w_dist_mat[250, w_dist_mat[250] > 0])
 plt.show()
 
 sorted_wd_mat = np.sort(w_dist_mat)
 test = sorted_wd_mat[-250:, :]
 test_mean = test.mean(axis=0)
-
-sns.distplot(test_mean[test_mean>0])
-plt.show()
-
-q_arr = np.linspace(0.05, 1.0, 20)
-q_mat = get_q_vals(q_arr, w_dist_mat)
-q_vals = get_q_vals2(q_arr, test_mean)
-
-plt.plot(q_vals)
-plt.show()
-
-q_arr = np.linspace(0.05, 1.0, 20)
-w_dist1 = np.log(w_dist_mat[0, w_dist_mat[0] > 0])
-w_dist2 = np.log(w_dist_mat[1, w_dist_mat[1] > 0])
-q_vals1 = get_q_vals(q_arr, w_dist1)
-q_vals2 = get_q_vals(q_arr, w_dist2)
-plt.plot(q_vals2 - q_vals1)
-plt.show()
-
-plt.plot(w_dist_mat[:, 800])
-plt.show()
 
 # seed = 1234123
 # rd.seed(seed)
