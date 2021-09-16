8564f3c4152f5490ca64ca4d665c587b6807985e
atecan00 <c.ates@unibas.ch>
2021-09-16 16:32:41 +0200

    set up experiments

--- dist_sim.py
+++ dist_sim.py
@@ -5,7 +5,7 @@ import concurrent.futures
 import csv
 
 if __name__ == '__main__':
-    n_sim = 100
+    n_sim = 8
     seed = 123456
     T = 1000
     lambda_LM_arr = np.array([1, 2.5, 5, 7.5, 10, 12.5, 15])
@@ -24,9 +24,19 @@ if __name__ == '__main__':
         parvals = par_vals_list[i]
         for parval in parvals:
             print("start {} simulations for {} = {} ....".format(n_sim, csv_name, parval))
-            results = counterfact_sim(seed, i, d_mwp_arr, n_sim, q_arr, T, parval)
-            diff = results[1,:, :] - results[0,:, :]
+            results1, results2 = counterfact_sim(seed, i, d_mwp_arr, n_sim, q_arr, T, parval)
+            diff = np.log(results1[1,:, :]) - np.log(results2[0,:, :])
             diff = np.insert(diff, 0, parval, axis=1)
-            with open('dist_sim_results/dist_sim_{}.csv'.format(csv_name), 'a', newline = '') as csvfile:
+            with open('dist_sim_results/dist_sim_qvals_{}.csv'.format(csv_name), 'a', newline = '') as csvfile:
                 filewriter = csv.writer(csvfile, delimiter = ',')
                 filewriter.writerows(diff)
+            for j in range(results2.shape[0]):
+                with open('dist_sim_results/dist_sim_mean_data_{}_{}.csv'.format(csv_name, j), 'a', newline = '') as csvfile:
+                    filewriter = csv.writer(csvfile, delimiter = ',')
+                    filewriter.writerow(["unemployment_rate", "nominal_GDP", "real_GDP", "mean_price",
+                                         "mean_wage", "median_real_wage", "mean_real_wage", "gini_coefficient",
+                                         "share_of_inactive", "share_of_refinanced", "unskilled_wage", "skilled_wages",
+                                         "wage_ratio_s_to_u", "C", "DC", "total_expenditure", "A_ratio_FtoH",
+                                         "mean_m", "mean_uc", "mean_delta", "tot_inv", "ur_unskilled",
+                                         "ur_skilled"])
+                    filewriter.writerows(results2[j, :, :].T)
--- dist_sim_tools.py
+++ dist_sim_tools.py
@@ -9,21 +9,24 @@ def run_model(arg):
 
     q_mat = get_q_vals(q_arr, w_dist_mat)
     q_vals = q_mat[-400:, :].mean(axis=0)
-    return q_vals
+    mean_data = data_mat[:,-400:].mean(axis=1)
+    return mean_data, q_vals
 
 
 def run_mp(args):
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        X = np.array(list(executor.map(run_model, args)))
-    q_vals_mat = X
-    return q_vals_mat
+        mean_d, q = np.array(list(executor.map(run_model, args)))
+    q_vals_mat = q
+    mean_data_mat = mean_d
+    return mean_data_mat, q_vals_mat
 
 def counterfact_sim(seed, ID, d_mwp_arr, n_sim, q_arr, T, parval):
-    results = np.zeros((d_mwp_arr.size, n_sim, q_arr.size))
+    results1 = np.zeros((d_mwp_arr.size, n_sim, q_arr.size)) # qvals
+    results2 = np.zeros((d_mwp_arr.size, 23, n_sim)) # mean_data
     for i in range(d_mwp_arr.size):
         rd.seed(seed)
         set_seed(seed)
         args = [(ID, q_arr, T, d_mwp_arr[i], parval) for j in range(n_sim)]
-        q_vals_mat = run_mp(args)
-        results[i, :, :] = q_vals_mat
-    return results
+        mean_data_mat, q_vals_mat = run_mp(args)
+        results1[i, :, :] = q_vals_mat
+    return results1, results2
