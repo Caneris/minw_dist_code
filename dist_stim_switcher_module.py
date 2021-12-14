8c5cd38f9ed6a87232d27b5aedabc66f4413640d
atecan00 <c.ates@unibas.ch>
2021-12-14 16:07:38 +0100

    set up simulations for different min_w_base values

--- dist_stim_switcher_module.py
+++ dist_stim_switcher_module.py
@@ -1,33 +1,31 @@
 from step_function_methods import run
 
-min_w_par = 1e-14
-
-def ID0(T, d_mwp, par_val):
-    data_mat, w_dist_mat = run(T=T, lambda_LM=par_val, min_w_par=min_w_par, d_mwp=d_mwp)
+def ID0(T, d_mwp, par_val, min_w_base):
+    data_mat, w_dist_mat = run(T=T, lambda_LM=par_val, d_mwp=d_mwp, min_w_par=min_w_base)
     return data_mat, w_dist_mat
 
-def ID1(T, d_mwp, par_val):
+def ID1(T, d_mwp, par_val, min_w_base):
     data_mat, w_dist_mat = run(T=T, min_w_par=par_val, d_mwp=d_mwp)
     return data_mat, w_dist_mat
 
-def ID2(T, d_mwp, par_val):
-    data_mat, w_dist_mat = run(T=T, sigma_w=par_val, min_w_par=min_w_par, d_mwp=d_mwp)
+def ID2(T, d_mwp, par_val, min_w_base):
+    data_mat, w_dist_mat = run(T=T, sigma_w=par_val, d_mwp=d_mwp, min_w_par=min_w_base)
     return data_mat, w_dist_mat
 
-def ID3(T, d_mwp, par_val):
-    data_mat, w_dist_mat = run(T=T, sigma_m=par_val, min_w_par=min_w_par, d_mwp=d_mwp)
+def ID3(T, d_mwp, par_val, min_w_base):
+    data_mat, w_dist_mat = run(T=T, sigma_m=par_val, d_mwp=d_mwp, min_w_par=min_w_base)
     return data_mat, w_dist_mat
 
-def ID4(T, d_mwp, par_val):
-    data_mat, w_dist_mat = run(T=T, N_app=par_val, min_w_par=min_w_par, d_mwp=d_mwp)
+def ID4(T, d_mwp, par_val, min_w_base):
+    data_mat, w_dist_mat = run(T=T, N_app=par_val, d_mwp=d_mwp, min_w_par=min_w_base)
     return data_mat, w_dist_mat
 
-def ID5(T, d_mwp, par_val):
-    data_mat, w_dist_mat = run(T=T, N_good=par_val, min_w_par=min_w_par, d_mwp=d_mwp)
+def ID5(T, d_mwp, par_val, min_w_base):
+    data_mat, w_dist_mat = run(T=T, N_good=par_val, d_mwp=d_mwp, min_w_par=min_w_base)
     return data_mat, w_dist_mat
 
-def ID6(T, d_mwp, par_val):
-    data_mat, w_dist_mat = run(T=T, gamma_s=par_val, min_w_par=min_w_par, d_mwp=d_mwp)
+def ID6(T, d_mwp, par_val, min_w_base):
+    data_mat, w_dist_mat = run(T=T, gamma_s=par_val, d_mwp=d_mwp, min_w_par=min_w_base)
     return data_mat, w_dist_mat
 
 switcher = {
