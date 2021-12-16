fa0e3cfad89b93678714e5c448387d7753acd740
atecan00 <c.ates@unibas.ch>
2021-12-16 14:36:28 +0100

    debugging gamma_s

--- main.py
+++ main.py
@@ -7,28 +7,31 @@ periods = T
 H = 250
 change_t = 500
 
+gamma_s_arr = np.arange(0.2, 0.8, 0.1)
 q_arr = np.linspace(0.01, 1.0, 100)
 d_mwp_arr = np.array([0.0, 0.6])
-results = np.zeros((2, q_arr.size))
+results = np.zeros((2, q_arr.size, gamma_s_arr.size))
 
 start = time.time()
 
 for i in range(d_mwp_arr.size):
-    seed = 1231
-    rd.seed(seed)
-    set_seed(seed)
-    data_mat, w_dist_mat = run(T=T, alpha_2=0.25, N_good=6, lambda_LM=10, sigma_m=0.35, sigma_w=0.40, sigma_delta=0.1,
-                               lambda_F=0.5, lambda_H=1.0, F=40, H=H, N_app=6, eta=1.5, min_w_par=1e-14, W_u=1, Ah=1,
-                               tol=1e-14, change_t=change_t, d_mwp=d_mwp_arr[i])
+    for j in range(len(gamma_s_arr)):
+        seed = 1231
+        rd.seed(seed)
+        set_seed(seed)
+        data_mat, w_dist_mat = run(T=T, alpha_2=0.25, N_good=6, lambda_LM=10, sigma_m=0.35, sigma_w=0.40, sigma_delta=0.1,
+                                   lambda_F=0.5, lambda_H=1.0, F=40, H=H, N_app=6, eta=1.5, min_w_par=1e-14, W_u=1, Ah=1,
+                                   tol=1e-14, change_t=change_t, d_mwp=d_mwp_arr[i], gamma_s=gamma_s_arr[j])
 
-    q_mat = get_q_vals(q_arr, w_dist_mat)
-    q_vals = q_mat[-100:,:].mean(axis=0)
-    results[i,:] = q_vals
+        q_mat = get_q_vals(q_arr, w_dist_mat)
+        q_vals = q_mat[-100:,:].mean(axis=0)
+        results[i,:,j] = q_vals
 
 end = time.time()
 print(end - start)
-
-plt.plot(results[1] - results[0])
+i = 4
+print(gamma_s_arr[i])
+plt.plot(results[1,:,i] - results[0,:,i])
 plt.show()
 
 # unemployment
